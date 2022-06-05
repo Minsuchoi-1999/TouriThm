6231850455a682d22b619467a7ac1824a6a52d17
Minsuchoi-1999 <jungsi1999@naver.com>
2022-06-05 18:03:34 +0900

    improve GDP

--- Crawling_Country.py
+++ Crawling_Country.py
@@ -12,6 +12,10 @@ country = BeautifulSoup(countrySearch.text, "html.parser")
 countryData = country.find('div', 'country_stage_box')
 countryData = countryData.find_all('a')
 
+with warnings.catch_warnings(record=True):
+    warnings.simplefilter("always")
+    df = pd.read_excel('국내총생산_20220605154742.xlsx', engine = 'openpyxl')
+
 
 for j in countryData:
     word = j.get_text()
@@ -41,6 +45,68 @@ for j in countryData:
         Capital = BeautifulSoup(CapitalSearch.text, "html.parser")
         Capitaldata = Capital.find("dl", "info_naflag").find('a')
         print(Capitaldata.get_text())
+
+    if(word == '남수단'):
+        print(12000)
+    elif(word == '니우에' or word == '쿡제도'):
+        word = '뉴질랜드'
+        dfnew = df[df['국가'] == word];
+        if(dfnew[2020].values[0] != '-'):
+            gdp = dfnew[2020].values[0]
+        elif(dfnew['2019'].values[0] != '-'):
+            gdp = dfnew['2019'].values[0]
+        else:
+            gdp = dfnew[2018].values[0]
+        print(gdp)
+    elif(word == '대만'):
+        print(759104)
+    elif(word == '모리타니아'):
+        print(7779)
+    elif(word == '베네수엘라'):
+        print(48600)
+    elif(word == '보스니아헤르체고비나'):
+        print(19790)
+    elif(word == '사이프러스'):
+        print(23800)
+    elif(word == '시리아'):
+        print(40400)
+    elif(word == '에리트레아'):
+         print(2065)
+    elif(word == '코소보'):
+        print(8810)
+    elif(word == '팔레스타인'):
+        print(15560)
+    elif(word == '호주'):
+        word = '오스트레일리아'
+        dfnew = df[df['국가'] == word];
+        if(dfnew[2020].values[0] != '-'):
+            gdp = dfnew[2020].values[0]
+        elif(dfnew['2019'].values[0] != '-'):
+            gdp = dfnew['2019'].values[0]
+        else:
+            gdp = dfnew[2018].values[0]
+        print(gdp)
+    elif(word == '홍콩(중국)'):
+        word = '홍콩'
+        dfnew = df[df['국가'] == word];
+        if(dfnew[2020].values[0] != '-'):
+            gdp = dfnew[2020].values[0]
+        elif(dfnew['2019'].values[0] != '-'):
+            gdp = dfnew['2019'].values[0]
+        else:
+            gdp = dfnew[2018].values[0]
+        print(gdp)
+        
+    else:
+        dfnew = df[df['국가'] == word];
+        if(dfnew[2020].values[0] != '-'):
+            gdp = dfnew[2020].values[0]
+        elif(dfnew['2019'].values[0] != '-'):
+            gdp = dfnew['2019'].values[0]
+        else:
+            gdp = dfnew[2018].values[0]
+        print(gdp)
+
 
     
 
